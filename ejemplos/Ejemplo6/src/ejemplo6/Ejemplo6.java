06c956d7d6e02e2fa3e19791cd35e08f0c8a4017
Daniel <danielp4p1@gmail.com>
2025-11-12 16:55:00 -0500

    commit

--- ejemplos/Ejemplo6/src/ejemplo6/Ejemplo6.java
+++ ejemplos/Ejemplo6/src/ejemplo6/Ejemplo6.java
@@ -18,14 +18,14 @@ public class Ejemplo6 {
         
         // Se declaran las variables
         int membresia = 25;
-        double porcentajeDcto = 0.5;
+        double porcentajeDcto = 0.05;
         String nombre;
         String apellido;
         String usuario;
         int edad;
         String parroquia;
         int diaPago;
-        double dcto = membresia * 0.5;
+        double dcto = membresia * porcentajeDcto;
         double dctoAplicado = membresia - dcto;
         
         // Se piden los datos por teclado
@@ -43,16 +43,16 @@ public class Ejemplo6 {
         System.out.println("Ingrese el dia de pago");
         diaPago = entrada.nextInt();
         
-        if ((diaPago>10) && (parroquia.equals("El Valle") || 
+        if ((diaPago<10) && (parroquia.equals("El Valle") || 
                 (parroquia.equals("El Sagrario")))) {
-            System.out.printf("Nombre: %s\nApellido: %s\nUsuario: %s\n"
-                    + "Edad: %d\nParroquia: %s\n Dia de Pago: %d\n"
+            System.out.printf("\nNombre: %s\nApellido: %s\nUsuario: %s\n"
+                    + "Edad: %d\nParroquia: %s\nDia de Pago: %d\n"
                     + "Descuento (aplicable):%.2f\nTotal a pagar:%.2f\n", 
                     nombre, apellido, usuario, edad, parroquia, 
                     diaPago, dcto, dctoAplicado);
-        } else {System.out.printf("Nombre: %s\nApellido: %s\nUsuario: %s\n"
-                    + "Edad: %d\nParroquia: %s\n Dia de Pago: %d\n"
-                    + "Descuento (NO aplicable):%.2f\nTotal a pagar:%.2f\n", 
+        } else {System.out.printf("\nNombre: %s\nApellido: %s\nUsuario: %s\n"
+                    + "Edad: %d\nParroquia: %s\nDia de Pago: %d\n"
+                    + "Descuento (NO aplicable):%.2f\nTotal a pagar:%d\n", 
                     nombre, apellido, usuario, edad, parroquia, 
                     diaPago, dcto, membresia);        
         }
